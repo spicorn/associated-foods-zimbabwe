ce87bfcaac616957fc1b8f0d66a5279816b7d65a
Dancel Mautsa <70059622+spicorn@users.noreply.github.com>
2023-12-08 10:49:43 +0200

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,19 @@
+const $counters = document.querySelectorAll('.counter');
+
+$counters.forEach($counter => {
+    $counter.innerText ='0';
+    const updateCounter = () => {
+        const target = Number(
+            $counter.getAttribute('data-target'),
+            );
+        const count = Number ($counter.innerText);
+        const increment = target / 200;
+        if (count < target) {
+            $counter.innerText = '${Math.ceil(count + increment)}';
+            setTimeout(updateCounter, 1);
+        } else {
+            $counter.innerText = target;
+        }
+    };
+       updateCounter();
+});
